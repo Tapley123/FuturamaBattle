648a970a4ba5640ab8b92a4ac83785a1bfc646b7
ConorTapley <34166407+ConorTapley@users.noreply.github.com>
2020-03-09 14:37:02 -0800

    more models

---
+++ Space Battle/Assets/Scripts/DeathstarMovement.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathstarMovement : MonoBehaviour
+{
+    public Vector3 rotationSpeed = new Vector3(0, 2, 2);
+
+    void Update()
+    {
+        this.transform.Rotate(rotationSpeed);
+    }
+}
