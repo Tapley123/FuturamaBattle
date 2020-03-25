83c1e87b3a77db32860c1a616fad3ef7b131e6cd
ConorTapley <34166407+ConorTapley@users.noreply.github.com>
2020-03-25 07:33:27 -0800

    shooting working with space bar

---
+++ Space Battle/Assets/Scripts/BulletBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletBehaviour : MonoBehaviour
+{
+    [SerializeField] private float deleteBulletTime = 5f;
+    [SerializeField] private float speed = 20f;
+
+    private void Awake()
+    {
+        StartCoroutine(DeleteBulletCoroutine(deleteBulletTime));
+    }
+
+    void Update()
+    {
+        transform.position += (Vector3.left * -1) * Time.deltaTime * speed;
+    }
+
+    IEnumerator DeleteBulletCoroutine(float time)
+    {
+        yield return new WaitForSeconds(time);
+        Destroy(this.gameObject);
+    }
+
+}
---
+++ Space Battle/Assets/Scripts/Shoot.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    public GameObject bullet;
+    public Transform bulletSpawn;
+
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.Space))
+        {
+            GameObject.Instantiate(bullet, bulletSpawn.transform.position, bulletSpawn.transform.rotation);
+        }
+    }
+}
