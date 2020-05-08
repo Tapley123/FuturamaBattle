26547e0ccbfe938be47c385467564ca2e7607c93
ConorTapley <34166407+ConorTapley@users.noreply.github.com>
2020-05-08 02:15:38 +0100

    Tiding code with loops

--- Space Battle/Assets/Finite State Machines/FallIntoFormation.cs
+++ Space Battle/Assets/Finite State Machines/FallIntoFormation.cs
@@ -8,36 +8,32 @@ public class FallIntoFormation : VFBase
     public float LerpValue = 0f;
     public float rotationLookAtDistance = 10f; //so the ships face a position a bit in front of their position to stop them spinning in place
 
-    //OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        base.OnStateEnter(animator, stateInfo, layerIndex);
+        base.OnStateEnter(animator, stateInfo, layerIndex); //call the enter state from the base class
     }
 
-    //OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
+        //rotation
+        for (int i = 0; i <= positions.Length - 1; i++)
+        {
+            ships[i].transform.LookAt(new Vector3(positions[i].x + rotationLookAtDistance, positions[i].y, positions[i].z)); //rotate to position
+        }
+
         //lerp to position
         if (LerpValue < 1)
         {
             LerpValue += Time.deltaTime / fallIntoPositionTime;
-            ships[0].transform.position = Vector3.Lerp(ships[0].transform.position, positions[0], LerpValue); //move to position
-            ships[1].transform.position = Vector3.Lerp(ships[1].transform.position, positions[1], LerpValue); //move to position
-            ships[2].transform.position = Vector3.Lerp(ships[2].transform.position, positions[2], LerpValue); //move to position
-            ships[3].transform.position = Vector3.Lerp(ships[3].transform.position, positions[3], LerpValue); //move to position
-            ships[4].transform.position = Vector3.Lerp(ships[4].transform.position, positions[4], LerpValue); //move to position
-            ships[5].transform.position = Vector3.Lerp(ships[5].transform.position, positions[5], LerpValue); //move to position
+            for(int i = 0; i <= ships.Length - 1; i++)
+            {
+                ships[i].transform.position = Vector3.Lerp(ships[i].transform.position, positions[i], LerpValue); //move to position
+            }
         }
         else
             LerpValue = 0;
-
-        //rotation
-        ships[0].transform.LookAt(new Vector3(positions[0].x + rotationLookAtDistance, positions[0].y, positions[0].z));
-        ships[1].transform.LookAt(new Vector3(positions[1].x + rotationLookAtDistance, positions[1].y, positions[1].z));
-        ships[2].transform.LookAt(new Vector3(positions[2].x + rotationLookAtDistance, positions[2].y, positions[2].z));
-        ships[3].transform.LookAt(new Vector3(positions[3].x + rotationLookAtDistance, positions[3].y, positions[3].z));
-        ships[4].transform.LookAt(new Vector3(positions[4].x + rotationLookAtDistance, positions[4].y, positions[4].z));
-        ships[5].transform.LookAt(new Vector3(positions[5].x + rotationLookAtDistance, positions[5].y, positions[5].z));
     }
 
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
