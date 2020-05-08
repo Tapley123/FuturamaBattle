325acf0f95b9ba11e539bdee5b4c99e91b712a99
ConorTapley <34166407+ConorTapley@users.noreply.github.com>
2020-05-08 15:07:51 +0100

    All Fire working with state machine

---
+++ Space Battle/Assets/Finite State Machines/VFormation/AllShoot.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AllShoot : VFBase
+{
+    private float elapsed = 0f;
+    private float timeInbetweenBullets = 0.3f; // time inbetween each bullet firing
+
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        base.OnStateEnter(animator, stateInfo, layerIndex); //call the enter state from the base class
+    }
+
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        elapsed += Time.deltaTime; // get time that elapsed
+        if (elapsed >= timeInbetweenBullets)// && shoot)
+        {
+            elapsed = elapsed % timeInbetweenBullets; //reset the timer
+            for(int i = 0; i < bulletSpawnPositions.Length; i++)
+            {
+                GameObject instance = GameObject.Instantiate(bullet, bulletSpawnPositions[i].position, bulletSpawnPositions[i].rotation);
+                instance.transform.forward = bulletSpawnPositions[i].forward;
+            }
+        }
+    }
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        
+    }
+}
---
+++ Space Battle/Assets/Finite State Machines/VFormation/FallIntoFormation.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallIntoFormation : VFBase
+{
+    public float distance = 10f;
+    public float LerpValue = 0f;
+    public float rotationLookAtDistance = 10f; //so the ships face a position a bit in front of their position to stop them spinning in place
+
+
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        base.OnStateEnter(animator, stateInfo, layerIndex); //call the enter state from the base class
+    }
+
+
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        //rotation
+        for (int i = 0; i <= formationPositions.Length - 1; i++)
+        {
+            ships[i].transform.LookAt(new Vector3(formationPositions[i].x + rotationLookAtDistance, formationPositions[i].y, formationPositions[i].z)); //rotate to position
+        }
+
+        //lerp to position
+        if (LerpValue < 1)
+        {
+            LerpValue += Time.deltaTime / fallIntoPositionTime;
+            for(int i = 0; i <= ships.Length - 1; i++)
+            {
+                ships[i].transform.position = Vector3.Lerp(ships[i].transform.position, formationPositions[i], LerpValue); //move to position
+            }
+        }
+        else
+            LerpValue = 0;
+
+        //check if the ships are in formation
+        float distance = Vector3.Distance(ships[0].transform.position, formationPositions[0]);
+        if (distance <= 1)
+            animator.SetBool("inFormation", true);
+        else
+            animator.SetBool("inFormation", false);
+    }
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        
+    }
+}
