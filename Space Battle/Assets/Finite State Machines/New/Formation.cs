15fdaabc12d6e0d445cf9c833db86d1c796c6aa8
ConorTapley <34166407+ConorTapley@users.noreply.github.com>
2020-05-15 16:11:56 -0800

    bla

--- Space Battle/Assets/Finite State Machines/New/Formation.cs
+++ Space Battle/Assets/Finite State Machines/New/Formation.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Formation : Base
 {
     private float lerpTime = 0.2f;
+    private float runTime = 5f;
 
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
@@ -18,6 +19,12 @@ public class Formation : Base
         for (int i = 0; i < ships.Count; i++)
         {
             ships[i].transform.position = Vector3.Lerp(ships[i].transform.position, positions[i], Time.time * lerpTime);
+        }
+        
+        if(ships[1].transform.position.x <= positions[1].x + 5)
+        {
+            //Debug.Log("In Position");
+            animator.SetTrigger("InFormation");
         }
     }
 }
