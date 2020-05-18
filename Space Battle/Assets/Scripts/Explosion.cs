fad31290960b2d00c6144f8352a007b2a108d7ca
ConorTapley <34166407+ConorTapley@users.noreply.github.com>
2020-05-18 14:45:02 +0100

    Fixed Camera Problems

---
+++ Space Battle/Assets/Scripts/Explosion.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour
+{
+    [SerializeField] private float deleteExplosionTime = 4f;
+    void Start()
+    {
+        StartCoroutine(DeleteExplosionCoroutine(deleteExplosionTime));
+    }
+
+    IEnumerator DeleteExplosionCoroutine(float time)
+    {
+        yield return new WaitForSeconds(time);
+        Destroy(this.gameObject);
+    }
+}
