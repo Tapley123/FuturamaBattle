1e0078a95664a6565430c615e5dc409ebd2cdc99
ConorTapley <34166407+ConorTapley@users.noreply.github.com>
2020-05-19 10:40:18 -0800

    Small touches

--- Space Battle/Assets/Finite State Machines/LoadMenu.cs
+++ Space Battle/Assets/Finite State Machines/LoadMenu.cs
@@ -8,6 +8,6 @@ public class LoadMenu : Base
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        SceneManager.LoadScene("Menu");
+        SceneManager.LoadScene(1);
     }
 }
