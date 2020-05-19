dc8b1f647afef366860488a87910827a3e128b19
ConorTapley <34166407+ConorTapley@users.noreply.github.com>
2020-05-19 18:19:09 +0100

    menu scene

---
+++ Space Battle/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    //public GameObject menu;
+
+    private void Awake()
+    {
+        //menu.SetActive(false);
+    }
+
+    public void ReplayButton()
+    {
+        SceneManager.LoadScene("Game");
+        //menu.SetActive(false);
+    }
+
+    public void QuitButton()
+    {
+        Application.Quit();
+    }
+}
