a2b484bcbc575a66002c3fa0b054d6be94a82d24
ConorTapley <34166407+ConorTapley@users.noreply.github.com>
2020-05-19 11:08:06 -0800

    Fix

--- Space Battle/Assets/Finite State Machines/New/Formation.cs
+++ Space Battle/Assets/Finite State Machines/New/Formation.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Formation : Base
 {
-    private float lerpTime = 0.2f;
+    private float lerpTime = 0.01f;
 
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
@@ -20,6 +20,8 @@ public class Formation : Base
             ships[i].transform.position = Vector3.Lerp(ships[i].transform.position, positions[i], Time.time * lerpTime);
         }
         
+
+        //check if in formation
         if(ships[1].transform.position.x <= positions[1].x + 5)
         {
             //Debug.Log("In Position");
