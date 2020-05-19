f2b3323fd63017944646938f7eca230ded02119f
ConorTapley <34166407+ConorTapley@users.noreply.github.com>
2020-05-19 15:16:18 +0100

    making the code more efficient

--- Space Battle/Assets/Finite State Machines/New/Base.cs
+++ Space Battle/Assets/Finite State Machines/New/Base.cs
@@ -29,18 +29,18 @@ public class Base : StateMachineBehaviour
         ships.Add(GameObject.Find("SchoolBus"));
         ships.Add(GameObject.Find("Tie_Fighter"));
 
+        //add the formation positions
         positions.Add(new Vector3(planetExpressTransform.position.x - line, planetExpressTransform.position.y, planetExpressTransform.position.z - row));
         positions.Add(new Vector3(planetExpressTransform.position.x + line, planetExpressTransform.position.y, planetExpressTransform.position.z - row));
         positions.Add(new Vector3(planetExpressTransform.position.x - (line * 2), planetExpressTransform.position.y, planetExpressTransform.position.z - (row * 2)));
         positions.Add(new Vector3(planetExpressTransform.position.x, planetExpressTransform.position.y, planetExpressTransform.position.z - (row * 2)));
         positions.Add(new Vector3(planetExpressTransform.position.x + (line * 2), planetExpressTransform.position.y, planetExpressTransform.position.z - (row * 2)));
 
-        bulletPosition.Add(GameObject.Find("0_BulletSpawn").transform);
-        bulletPosition.Add(GameObject.Find("1_BulletSpawn").transform);
-        bulletPosition.Add(GameObject.Find("2_BulletSpawn").transform);
-        bulletPosition.Add(GameObject.Find("3_BulletSpawn").transform);
-        bulletPosition.Add(GameObject.Find("4_BulletSpawn").transform);
-        bulletPosition.Add(GameObject.Find("5_BulletSpawn").transform);
+        //add the bullet spawn Positions
+        for(int i = 0; i < 6; i++)
+        {
+            bulletPosition.Add(GameObject.Find(i + "_BulletSpawn").transform);
+        }
 
         guns.Add(GameObject.Find("Gun_PlanetExpress"));
         guns.Add(GameObject.Find("Gun_MobShip"));
@@ -49,33 +49,11 @@ public class Base : StateMachineBehaviour
         guns.Add(GameObject.Find("Gun_SchoolBus"));
         guns.Add(GameObject.Find("Gun_TieFighter"));
 
-        deathStars.Add(GameObject.Find("0_Target"));
-        deathStars.Add(GameObject.Find("0_Target (1)"));
-        deathStars.Add(GameObject.Find("0_Target (2)"));
-        deathStars.Add(GameObject.Find("0_Target (3)"));
-        deathStars.Add(GameObject.Find("0_Target (4)"));
-        deathStars.Add(GameObject.Find("0_Target (5)"));
-        deathStars.Add(GameObject.Find("0_Target (6)"));
-
-        targets.Add(GameObject.Find("0_Target").GetComponent<DeathstarMovement>());
-        targets.Add(GameObject.Find("0_Target (1)").GetComponent<DeathstarMovement>());
-        targets.Add(GameObject.Find("0_Target (2)").GetComponent<DeathstarMovement>());
-        targets.Add(GameObject.Find("0_Target (3)").GetComponent<DeathstarMovement>());
-        targets.Add(GameObject.Find("0_Target (4)").GetComponent<DeathstarMovement>());
-        targets.Add(GameObject.Find("0_Target (5)").GetComponent<DeathstarMovement>());
-        targets.Add(GameObject.Find("0_Target (6)").GetComponent<DeathstarMovement>());
-
-    }
-
-    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
-    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    {
-
-    }
-
-    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
-    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    {
-
+        //add deathstars and targets
+        for (int i = 0; i < 6; i++)
+        {
+            deathStars.Add(GameObject.Find("0_Target (" + i + ")"));
+            targets.Add(GameObject.Find("0_Target (" + i + ")").GetComponent<DeathstarMovement>());
+        }
     }
 }
